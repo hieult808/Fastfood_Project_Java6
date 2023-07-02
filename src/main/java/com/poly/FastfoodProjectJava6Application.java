ea64b5cd2837629b905e7857f82eb117f22c3cc3
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-07-03 00:29:28 +0700

    firts comit

---
+++ src/main/java/com/poly/FastfoodProjectJava6Application.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodProjectJava6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodProjectJava6Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/poly/controller/indexController.java
@@ -0,0 +1,68 @@
+package com.poly.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class indexController {
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		return "index";
+	}
+	
+	@GetMapping("/shop")
+	public String shop(Model model) {
+		return "shop";
+	}
+	
+	@GetMapping("/cart")
+	public String cart(Model model) {
+		return "cart";
+	}
+	
+	@GetMapping("/checkout")
+	public String checkout(Model model) {
+		return "checkout";
+	}
+	
+	@GetMapping("/contact")
+	public String contact(Model model) {
+		return "contact";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		return "about";
+	}
+	
+	@GetMapping("/news")
+	public String news(Model model) {
+		return "news";
+	}
+	
+	@GetMapping("/single-news")
+	public String singlenews(Model model) {
+		return "single-news";
+	}
+	
+	@GetMapping("/single-product")
+	public String singleproduct(Model model) {
+		return "single-product";
+	}
+	
+	@GetMapping("/profile")
+	public String indexAdmin(Model model) {
+		return "admin/profile";
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+
+}
