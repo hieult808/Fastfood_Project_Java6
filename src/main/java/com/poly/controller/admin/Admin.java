8c3e8c5d9f39d19e7ad3dad5e23b458480e5b5ed
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-07-13 17:46:40 +0700

    Upload templace

--- src/main/java/com/poly/controller/admin/Admin.java
+++ src/main/java/com/poly/controller/admin/Admin.java
@@ -6,15 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Admin {
-	@GetMapping("/profile")
+	@GetMapping("profile")
 	public String indexAdmin(Model model) {
 		return "admin/profile";
 	}
 	
-	@GetMapping("/item")
+	@GetMapping("category")
 	public String index2(Model model) {
 		return "admin/basic-table";
 	}
 	
+	@GetMapping("admin")
+	public String index3(Model model) {
+		return "admin/dashboard";
+	}
+	
+	@GetMapping("item")
+	public String index4(Model model) {
+		return "admin/profile";
+	}
 	
 }
