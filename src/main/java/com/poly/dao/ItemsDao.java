cfe76ddc870daec9a21f5bd451b5bf74d0e25566
Lê Trung Hiếu <133332622+LeHieu88@users.noreply.github.com>
2023-08-03 17:56:45 +0700

    updata dao

--- src/main/java/com/poly/dao/ItemsDao.java
+++ src/main/java/com/poly/dao/ItemsDao.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.poly.entity.Item;
 
-public interface ItemDao extends JpaRepository<Item, Integer> {
+public interface ItemsDao extends JpaRepository<Item, Integer> {
 
 }
